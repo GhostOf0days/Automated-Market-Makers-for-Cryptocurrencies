c3bdfba0fb81ceae6fbfa34e13cfe265adc4e06e
Anish Dhanasekaran <98651890+nepthius@users.noreply.github.com>
2024-03-05 18:28:57 -0500

    Add specific tick data/coins

--- data/createTardisData.py
+++ data/createTardisData.py
@@ -1,37 +1,43 @@
 import asyncio
 from tardis_client import TardisClient, Channel
+from tardis_dev import datasets, get_exchange_details
 import json
-import dataUtils
-from . import JSONPATH
+import csv
+from dataUtils import *
+from data import JSONPATH
 
-tardisKey = dataUtils.readCredentials(JSONPATH)
-assert isinstance(tardisKey, str) == True
-
-print(tardisKey)
-
-filename = "put file name here"
-
-print(tardisKey)
-
-tardis_client = TardisClient(api_key = tardisKey)
-
-async def replay():
-    # replay method returns Async Generator
-    messages = tardis_client.replay(
-        exchange="exchange goes here", # examples: coinbase, binance
-        from_date="from date goes here", # example: 2024-02-18
-        to_date="to data goes here", # example: 2024-02-25
-        filters=[Channel(name="csv file name", symbols=["symbol goes here"])], # example csv file: bitcoin_price_index.csv. example symbol: btscusdt.
+tardisKey = readCredentials(JSONPATH)
+DOWNLOADED = True
+if __name__ == "__main__":
+    if not DOWNLOADED:
+        datasets.download(
+            # one of https://api.tardis.dev/v1/exchanges with supportsDatasets:true - use 'id' value
+            exchange="binance-futures",
+            # accepted data types - 'datasets.symbols[].dataTypes' field in https://api.tardis.dev/v1/exchanges/deribit,
+            # or get those values from 'deribit_details["datasets"]["symbols][]["dataTypes"] dict above
+            data_types=["incremental_book_L2", "trades"],
+            # change date ranges as needed to fetch full month or year for example
+            from_date="2024-01-02",
+            # to date is non inclusive
+            to_date="2024-01-02",
+            # accepted values: 'datasets.symbols[].id' field in https://api.tardis.dev/v1/exchanges/deribit
+            symbols=["BTCUSDT", "ETHUSDT","BNBUSDT"],
+            # (optional) your API key to get access to non sample data as well
+            api_key=tardisKey,
+            # (optional) path where data will be downloaded into, default dir is './datasets'
+            download_dir="./datasets",
+            # (optional) - one can customize downloaded file name/path (flat dir strucure, or nested etc) - by default function 'default_file_name' is used
+            # get_filename=default_file_name,
+            # (optional) file_name_nested will download data to nested directory structure (split by exchange and data type)
+            # get_filename=file_name_nested,
         )
-    
-    with open("./csv_file_name.csv", mode="w") as csv_file: # example: ./bitcoin_price_index.cdv
-        fieldnames = ["symbol", "price", "timestamp"]
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-        writer.writeheader()
-        async for local_timestamp, message in messages:
-            data = message["params"]["data"]
-            writer.writerow({"symbol": data["index_name"], "price": data["price"], "timestamp": data["timestamp"]})
-
-    print("finished")
-
-asyncio.run(replay())
+    incL2Files = getFileNames("./datasets", 'incremental_book_L2')
+    tradeFiles = getFileNames("./datasets", 'trades')
+    coinNames = ["BTCUSDT", "ETHUSDT", "BNBUSDT"]
+    dates = getDatesFromFiles(incL2Files)
+    for coin in coinNames:
+        for date in dates:
+            incL2File = getFileNameByCoinAndDate(incL2Files,coin,date)
+            tradeFile = getFileNameByCoinAndDate(tradeFiles,coin,date)
+            wrtPath = getWrtPath(coin,date,'./datasets')
+            prepareDataFromTardis(incL2File,tradeFile,wrtPath)
