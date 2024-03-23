398df82a09ade7ee4f11c2e38af57cca8bf89378
Anish Dhanasekaran <98651890+nepthius@users.noreply.github.com>
2024-03-23 11:50:21 -0400

    Create riskFactor.py

---
+++ factorRisk/riskFactor.py
@@ -0,0 +1,48 @@
+import numpy as np
+import pandas as pd
+
+STOREPath =  "/Users/maoguan/workspace/RL-AMM/data/factorData/"
+
+
+class riskFactor(object):
+
+    def __init__(self, name, componentSymbols, freq='1m'):
+        self._factorName = name
+        self._symbols = componentSymbols
+        self._freq = '1m'
+        return
+
+    @property
+    def name(self):
+        return self._factorName
+
+    @property
+    def symbols(self):
+        return self._symbols
+
+    @property
+    def freq(self):
+        return self._freq
+
+    @property
+    def factorRetTS(self):
+        return self._factorTS
+
+    def getFactorReturnTS(self, data: pd.DataFrame) -> pd.DataFrame:
+        """
+        Calculate risk factor returns given frequency and symbols
+        Parameters
+        ----------
+        data: a panel dataframe with all symbols and their corresponding returns
+
+        Returns: a timeSeries table of risk factor returns and time
+        -------
+
+        """
+        factorRet = data[data['symbol'].isin(self._symbols)].groupby("origin_time").agg({'return': np.mean})
+        factorRet.rename(columns={'return': self._factorName}, inplace=True)
+        self._factorTS = factorRet
+        return
+
+    def storeData(self):
+        self._factorTS.to_csv(STOREPath + self._factorName + ".csv", index=False)
